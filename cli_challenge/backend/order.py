5523c44a88891dafffaf4b9a155d4112298ef0f0
joe <josephfielding03@gmail.com>
2024-04-06 21:31:06 -0400

    constants for order states

--- cli_challenge/backend/order.py
+++ cli_challenge/backend/order.py
@@ -1,7 +1,16 @@
-from product import Product
+from enum import Enum
 
+from product import Product
+from rail_system import *
 
 class Order:
+    NOT_ORDERED = 0
+    IN_BIN = 1
+    ON_RAIL_CAR = 2
+    ON_SHIP = 3
+    DELIVERED = 4
+
     def __init__(self, items, date):
         self.items = items
         self.date = date
+        self.state = Order.NOT_ORDERED
