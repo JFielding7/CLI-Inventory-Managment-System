a6692f58316b9a8cc6c6d85255e2f7a860c5ce58
nmbar <nmbarton1@loyola.edu>
2024-04-07 05:37:02 -0400

    starting bin display

--- cli_challenge/frontend/ui.py
+++ cli_challenge/frontend/ui.py
@@ -1,11 +1,13 @@
 from PyQt6 import QtWidgets, uic
 import sys
 
+from PyQt6.QtWidgets import QTableWidgetItem, QTreeWidgetItem
 from PyQt6.QtWidgets import QTableWidgetItem, QPushButton
 
 from cli_challenge.backend.bin import Bin
 from cli_challenge.backend.database import Database
 from cli_challenge.backend.order import Order
+from cli_challenge.backend.elevator import Elevator
 
 class Ui(QtWidgets.QMainWindow):
 
@@ -18,34 +20,37 @@ class Ui(QtWidgets.QMainWindow):
     def populate_table(self, orders: list[Order]):
         self.order_table.setRowCount(len(orders))
         for i in range(len(orders)):
-            button = QPushButton("Advance order")
-            button.clicked.connect(self.p)
-            self.order_table.setIndexWidget(self.order_table.model().index(i, 0), button)
-            # item0 = QTableWidgetItem(str(orders[i].id))
-            # item1 = QTableWidgetItem(str(orders[i].status_str()))
-            # item2 = QTableWidgetItem(str(orders[i].date))
-            # item3 = QTableWidgetItem(str(orders[i].cost))
-            # item4 = QTableWidgetItem(str(orders[i].revenue))
-            # item5 = QTableWidgetItem(str(orders[i].revenue - orders[i].cost))
-            # self.order_table.setItem(i, 0, item0)
-            # self.order_table.setItem(i, 1, item1)
+            strs = [orders[i].id, orders[i].status_str(), orders[i].date, f"${orders[i].cost:.2f}", f"${orders[i].revenue:.2f}", f"${orders[i].revenue-orders[i].cost:.2f}"]
+            for j, p in enumerate(map(lambda i: QTableWidgetItem(str(i)), strs)):
+                self.order_table.setItem(i, j, p)
+            self.total_profit.setText(f"Total Profit: ${Order.TOTAL_PROFIT:.2f}")
 
-            # self.order_table.setItem(i, 2, item2)
-            # self.order_table.setItem(i, 3, item3)
-            # self.order_table.setItem(i, 4, item4)
-            # self.order_table.setItem(i, 5, item5)
+            button = QPushButton("Advance order")
+            #button.clicked.connect(self.p)
+            self.order_table.setIndexWidget(self.order_table.model().index(i, 0), button)
         self.total_profit.setText(f"Your Total Profit Is: ${Order.TOTAL_PROFIT:.2f}")
 
     def populate_tree(self, bins: list[Bin]):
         for i in range(len(bins)):
-            pass
-
-    def p(self):
-        print("Fuck you")
+            item = ["bin "+str(i+1), str(bins[i].weight)]
+            q = QTreeWidgetItem(item)
+            self.bins.addTopLevelItem(q)
+            #for item in bins[i].items:
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
     window = Ui()  # Create an instance of our class
-    window.populate_table(Database.load_database())
+    orders = Database.load_database()
+    window.populate_table(orders)
+
+    elevator = Elevator()
+    products = []
+    for order in orders:
+        for i in range(len(order.items)):
+            products.append(order.items[i])
+    elevator.receive(products)
+    #elevator.send({0: 8.7, 1: 5})
+    window.populate_tree(elevator.bins)
+
     app.exec()  # Start the application
