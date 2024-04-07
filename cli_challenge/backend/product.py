969a5617f104c07924a02b9b772bb4b3540d4e61
nmbar <nmbarton1@loyola.edu>
2024-04-07 10:48:25 -0400

    removed inherited classes

--- cli_challenge/backend/product.py
+++ cli_challenge/backend/product.py
@@ -7,41 +7,11 @@ class Product:
 
     @staticmethod
     def get_BPMT(ID: int) -> float:
-        match ID:
-            case 0:
-                return Barley.BPMT
-            case 1:
-                return Corn.BPMT
-            case 2:
-                return Wheat.BPMT
-            case _:
-                return -1
+        barley = 45.9296
+        corn = 39.368
+        wheat = 36.7437
+        return [barley, corn, wheat][ID]
 
     @staticmethod
     def product_name(ID: int) -> str:
         return ["Barley", "Corn", "Wheat"][ID]
-
-
-
-class Barley(Product):
-    ID = 0
-    BPMT = 45.9296
-
-    def __init__(self, bushels: float, ID=ID, BPMT=BPMT):
-        super().__init__(bushels, ID, BPMT)
-
-
-class Corn(Product):
-    ID = 1
-    BPMT = 39.368
-
-    def __init__(self, bushels: float, ID=ID, BPMT=BPMT):
-        super().__init__(bushels, ID, BPMT)
-
-
-class Wheat(Product):
-    ID = 2
-    BPMT = 36.7437
-
-    def __init__(self, bushels: float, ID=ID, BPMT=BPMT):
-        super().__init__(bushels, ID, BPMT)
