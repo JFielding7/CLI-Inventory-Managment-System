e7a569ea585d1a43fa18abc66b92407bad6312d8
kyle <kylerichards32@gmail.com>
2024-04-06 23:16:12 -0400

    Add ui starter

---
+++ cli_challenge/frontend/ui.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('frontend.ui', self)  # Load the .ui file
+        self.show()  # Show the GUI
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
+    window = Ui()  # Create an instance of our class
+    app.exec_()  # Start the application
